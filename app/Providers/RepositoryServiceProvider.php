22a50ee5620a18e8ec18b508ab93fc07cf5dbb60
AJMBallesteros <ajmunbal@gmail.com>
2023-01-16 17:56:27 +0100

    Remove unused dependencies

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,12 +2,8 @@
 
 namespace App\Providers;
 
-use App\src\domain\Blueflag\BlueflagRepositoryInterface;
 use App\src\domain\InvoiceRepositoryInterface;
-use App\src\domain\Municipality\MunicipalityRepositoryInterface;
-use App\src\infrastructure\Blueflag\BlueflagRepository;
 use App\src\infrastructure\InvoiceRepository;
-use App\src\infrastructure\Municipality\MunicipalityRepository;
 use Illuminate\Support\ServiceProvider;
 
 class RepositoryServiceProvider extends ServiceProvider
